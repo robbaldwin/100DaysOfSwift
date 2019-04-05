75c023a15143016718bb46b9812c63a79457a2b0
Rob Baldwin <rob@robbaldwin.net>
2019-04-05 14:12:41 +0100

    Corrected code committed in error

--- C05 CountryFacts/CountryFacts/ViewController.swift
+++ C05 CountryFacts/CountryFacts/ViewController.swift
@@ -49,7 +49,7 @@ final class ViewController: UITableViewController {
         guard let vc = storyboard?.instantiateViewController(withIdentifier: "DetailViewController") as? DetailViewController else {
             fatalError("Unable to instantiate DetailViewController")
         }
-        vc.countries = countries
+        vc.country = countries[indexPath.row]
         navigationController?.pushViewController(vc, animated: true)
     }
 }
