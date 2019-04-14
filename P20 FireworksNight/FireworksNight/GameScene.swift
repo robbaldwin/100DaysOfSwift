ff561bb6857e1135c31922485da8dba9e38921a1
Rob Baldwin <rob@robbaldwin.net>
2019-04-14 16:37:08 +0100

    P20 with challenges completed

--- P20 FireworksNight/FireworksNight/GameScene.swift
+++ P20 FireworksNight/FireworksNight/GameScene.swift
@@ -11,17 +11,23 @@ import SpriteKit
 class GameScene: SKScene {
     
     var gameTimer: Timer?
+    var numberOfLaunches = 0
     var fireworks = [SKNode]()
     
     let leftEdge = -22
     let bottomEdge = -22
     let rightEdge = 1024 + 22
     
+    var scoreLabel: SKLabelNode!
     var score = 0 {
+        // Challenge 1: For an easy challenge try adding a score label that updates as the player’s score changes.
         didSet {
-            // your code here
+            scoreLabel.text = "Score: \(score)"
         }
     }
+    
+    var gameOverLabel: SKLabelNode!
+    var playAgainLabel: SKLabelNode!
     
     override func didMove(to view: SKView) {
         
@@ -30,6 +36,23 @@ class GameScene: SKScene {
         background.blendMode = .replace
         background.zPosition = -1
         addChild(background)
+        
+        // Challenge 1: For an easy challenge try adding a score label that updates as the player’s score changes.
+        scoreLabel = SKLabelNode(text: "Score: 0")
+        scoreLabel.fontName = "MarkerFelt-Thin"
+        scoreLabel.fontSize = 40
+        scoreLabel.fontColor = .white
+        scoreLabel.position = CGPoint(x: 850, y: 700)
+        addChild(scoreLabel)
+        
+        startGame()
+    }
+    
+    func startGame() {
+        
+        score = 0
+        numberOfLaunches = 0
+        fireworks.removeAll()
         
         gameTimer = Timer.scheduledTimer(timeInterval: 6, target: self, selector: #selector(launchFireworks), userInfo: nil, repeats: true)
     }
@@ -72,6 +95,16 @@ class GameScene: SKScene {
     func launchFireworks() {
         let movementAmount: CGFloat = 1800
         
+        
+        // Challenge 2: Make the game end after a certain number of launches. You will need to use the invalidate() method of Timer to stop it from repeating.
+        numberOfLaunches += 1
+        
+        if numberOfLaunches > 5 {
+            gameTimer?.invalidate()
+            gameOver()
+            return
+        }
+
         switch Int.random(in: 0...3) {
         case 0:
             // fire five, straight up
@@ -116,6 +149,12 @@ class GameScene: SKScene {
         let location = touch.location(in: self)
         let nodesAtPoint = nodes(at: location)
         
+        if nodesAtPoint.contains(where: { $0.name == "playAgain" }) {
+            gameOverLabel.removeFromParent()
+            playAgainLabel.removeFromParent()
+            startGame()
+        }
+        
         for case let node as SKSpriteNode in nodesAtPoint {
             guard node.name == "firework" else { continue }
             
@@ -150,5 +189,79 @@ class GameScene: SKScene {
                 firework.removeFromParent()
             }
         }
+    }
+    
+    func explode(firework: SKNode) {
+        
+        guard let emitter = SKEmitterNode(fileNamed: "explode") else { return }
+        emitter.position = firework.position
+        addChild(emitter)
+
+        // Challenge 3: Use the waitForDuration and removeFromParent actions in a sequence to make sure explosion particle emitters are removed from the game scene when they are finished.
+        let remove = SKAction.run {
+            emitter.removeFromParent()
+        }
+        
+        emitter.run(SKAction.sequence([SKAction.wait(forDuration: 2.0), remove]))
+        
+        firework.removeFromParent()
+    }
+    
+    func explodeFireworks() {
+        var numExploded = 0
+        
+        for (index, fireworkContainer) in fireworks.enumerated().reversed() {
+            guard let firework = fireworkContainer.children.first as? SKSpriteNode else { continue }
+            
+            if firework.name == "selected" {
+                // destroy this firework!
+                explode(firework: fireworkContainer)
+                fireworks.remove(at: index)
+                numExploded += 1
+            }
+        }
+        
+        switch numExploded {
+        case 0:
+            // nothing – rubbish!
+            break
+        case 1:
+            score += 200
+        case 2:
+            score += 500
+        case 3:
+            score += 1500
+        case 4:
+            score += 2500
+        default:
+            score += 4000
+        }
+    }
+    
+    func gameOver() {
+        
+        gameOverLabel = SKLabelNode(text: "Game Over")
+        gameOverLabel.fontName = "MarkerFelt-Thin"
+        gameOverLabel.fontSize = 70
+        gameOverLabel.fontColor = .white
+        gameOverLabel.position = CGPoint(x: 512, y: 384)
+        gameOverLabel.xScale = 0.001
+        gameOverLabel.yScale = 0.001
+        addChild(gameOverLabel)
+        
+        let gameOverAppear = SKAction.scale(to: 1.0, duration: 0.5)
+        gameOverLabel.run(gameOverAppear)
+        
+        playAgainLabel = SKLabelNode(text: "Play Again")
+        playAgainLabel.fontName = "MarkerFelt-Thin"
+        playAgainLabel.fontSize = 50
+        playAgainLabel.fontColor = .white
+        playAgainLabel.position = CGPoint(x: 512, y: -50)
+        playAgainLabel.name = "playAgain"
+        addChild(playAgainLabel)
+        
+        let playAgainAppear = SKAction.move(to: CGPoint(x: 512, y: 70), duration: 0.5)
+        
+        playAgainLabel.run(SKAction.sequence([SKAction.wait(forDuration: 1.0), playAgainAppear]))
     }
 }
