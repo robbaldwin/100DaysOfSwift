bbaac44413ac7a5718906aa6e185028f9d1ac0b3
Rob Baldwin <rob@robbaldwin.net>
2019-05-12 12:59:52 +0100

    Add P31

---
+++ P31 Multibrowser/Multibrowser/ViewController.swift
@@ -0,0 +1,145 @@
+//
+//  ViewController.swift
+//  Multibrowser
+//
+//  Created by Rob Baldwin on 12/05/2019.
+//  Copyright © 2019 Rob Baldwin. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate, UITextFieldDelegate, UIGestureRecognizerDelegate {
+    
+    @IBOutlet var addressBar: UITextField!
+    @IBOutlet var stackView: UIStackView!
+    
+    weak var activeWebView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setDefaultTitle()
+        
+        let add = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addWebView))
+        let delete = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(deleteWebView))
+        navigationItem.rightBarButtonItems = [delete, add]
+    }
+    
+    func setDefaultTitle() {
+        title = "Multibrowser"
+    }
+    
+    @objc func addWebView() {
+        let webView = WKWebView()
+        webView.navigationDelegate = self
+        
+        stackView.addArrangedSubview(webView)
+        
+        let url = URL(string: "https://www.apple.com")!
+        webView.load(URLRequest(url: url))
+        
+        webView.layer.borderColor = UIColor.blue.cgColor
+        selectWebView(webView)
+        
+        let recognizer = UITapGestureRecognizer(target: self, action: #selector(webViewTapped))
+        recognizer.delegate = self
+        webView.addGestureRecognizer(recognizer)
+        
+    }
+    
+    func selectWebView(_ webView: WKWebView) {
+        for view in stackView.arrangedSubviews {
+            view.layer.borderWidth = 0
+        }
+        
+        activeWebView = webView
+        webView.layer.borderWidth = 3
+    }
+    
+    @objc func webViewTapped(_ recognizer: UITapGestureRecognizer) {
+        if let selectedWebView = recognizer.view as? WKWebView {
+            selectWebView(selectedWebView)
+        }
+    }
+
+    @objc func deleteWebView() {
+        // safely unwrap our webview
+        if let webView = activeWebView {
+            if let index = stackView.arrangedSubviews.firstIndex(of: webView) {
+                // we found the current webview in the stack view! Remove it from the stack view
+                stackView.removeArrangedSubview(webView)
+                
+                // now remove it from the view hierarchy – this is important!
+                webView.removeFromSuperview()
+                
+                if stackView.arrangedSubviews.count == 0 {
+                    // go back to our default UI
+                    setDefaultTitle()
+                } else {
+                    // convert the Index value into an integer
+                    var currentIndex = Int(index)
+                    
+                    // if that was the last web view in the stack, go back one
+                    if currentIndex == stackView.arrangedSubviews.count {
+                        currentIndex = stackView.arrangedSubviews.count - 1
+                    }
+                    
+                    // find the web view at the new index and select it
+                    if let newSelectedWebView = stackView.arrangedSubviews[currentIndex] as? WKWebView {
+                        selectWebView(newSelectedWebView)
+                    }
+                }
+            }
+        }
+    }
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+    
+    func updateUI(for webView: WKWebView) {
+        title = webView.title
+        addressBar.text = webView.url?.absoluteString ?? ""
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        addressBar.text = "https://"
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        guard let address = addressBar.text else { return true }
+
+        guard address.contains("https://") else {
+            let ac = UIAlertController(title: "Invalid URL", message: nil, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+            return true
+        }
+        
+        if let webView = activeWebView {
+            
+            if let url = URL(string: address) {
+                webView.load(URLRequest(url: url))
+            }
+        }
+        
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        if traitCollection.horizontalSizeClass == .compact {
+            stackView.axis = .vertical
+        } else {
+            stackView.axis = .horizontal
+        }
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        if webView == activeWebView {
+            updateUI(for: webView)
+        }
+    }
+}
---
+++ P31 Multibrowser/README.md
@@ -0,0 +1,3 @@
+# Project 31 of Hacking With Swift
+
+## Multibrowser
